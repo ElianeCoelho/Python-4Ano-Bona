03d14b9d79469ceb924484bc03a7bdba43e3bd2f
eliaCoelho <ellianee@hotmail.com>
2024-02-29 09:33:52 -0300

    first commit

---
+++ saidaDeDados.py
@@ -0,0 +1,36 @@
+print("======== Calculos aritméticos=========")
+
+#adição
+print(1+1)
+
+#subtração
+print(10-2)
+
+#Multiplicação
+print(10*3)
+
+#Divisão
+print(12/4)
+
+#Exponenciação
+print(5 ** 2)
+
+#Modulo
+print(10 % 2)
+
+print("=====Calculos aritmétcios com variáveis========")
+x = 20
+y = 5
+
+print(x / y)
+
+print("==========Operadores de Comparação ==========")
+valor_1 = 500
+valor_2 = 250
+
+print(valor_1 == valor_2)
+print(valor_1 != valor_2)
+print(valor_1 >= valor_2)
+print(valor_1 <= valor_2)
+print(valor_2 > valor_2)
+print(valor_2 < valor_2)
